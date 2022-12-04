575e2cd27eca6a842d8058cfaa937b23c8ba814f
jvieirah <me.contrate.aero@gmail.com>
2022-12-04 02:28:59 -0300

    Refactor: env variables added to deploy

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -11,10 +11,10 @@ const suffix = {
 };
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
+  host: process.env.MYSQL_URL,
+  port: process.env.MYSQLPORT,
   database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
+    `${process.env.MYSQL_DB_NAME || 'Backend-app'}${suffix[environment] || suffix.test}`,
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
   dialect: 'mysql',
