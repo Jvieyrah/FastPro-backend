10182749cf35506a649812751dc2db407e449464
jvieirah <me.contrate.aero@gmail.com>
2022-12-04 22:59:49 -0300

    FEAT: implementation of Cors on app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 require('express-async-errors');
 const express = require('express');
 const userRouter = require('./routes/userRouter');
+const cors = require('cors');
 const errorMiddleware = require('./middleware/errorMiddleware');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.get('/helloAPI', (_req, res) => res.status(418).send({ message: 'Hello User' }));
 app.use('/', userRouter);
