46547f5a3bf0154db824989f8c54aecbb4cb273b
jvieirah <me.contrate.aero@gmail.com>
2022-12-08 14:47:27 -0300

    simple test

--- src/service/PokemonService.js
+++ src/service/PokemonService.js
@@ -9,13 +9,13 @@ class PokemonService {
     this.url = 'https://pokeapi.co/api/v2';
   }
   importPokemons = async (offset) => {
-    const response = await axios
-      .get(`${this.url}/pokemon?limit=10&offset=${offset}`);
-    const { results } = response;
-    const pokemons = results.map((pokemon) => ({
-      name: pokemon.name,
-      id: pokemon.url.slice(25).replace('/', ''),
-    }));
+    // const response = await axios
+    //   .get(`${this.url}/pokemon?limit=10&offset=${offset}`);
+    // const { results } = response;
+    // const pokemons = results.map((pokemon) => ({
+    //   name: pokemon.name,
+    //   id: pokemon.url.slice(25).replace('/', ''),
+    // }));
     return ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
   }
 }
