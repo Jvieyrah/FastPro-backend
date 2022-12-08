5080af90927e2b5126facc2b907175706c0b7db8
jvieirah <me.contrate.aero@gmail.com>
2022-12-08 14:26:02 -0300

    simple test

--- src/service/PokemonService.js
+++ src/service/PokemonService.js
@@ -2,16 +2,21 @@ const getPokemons = require('../axios/config');
 const tokenManager = require('../helpers/tokenManager.js');
 const StructuredError = require('../errors/StructuredError');
 const UserService = require('./UserService');
+const axios = require('axios').default;
 
 class PokemonService {
+  constructor() {
+    this.url = 'https://pokeapi.co/api/v2';
+  }
   importPokemons = async (offset) => {
-    const response = await getPokemons.get(`/pokemon?limit=10&offset=${offset}`);
+    const response = await axios
+      .get(`${this.url}/pokemon?limit=10&offset=${offset}`);
     const { results } = response;
     // const pokemons = results.map((pokemon) => ({
     //   name: pokemon.name,
     //   id: pokemon.url.slice(25).replace('/', ''),
     // }));
-    return response;
+    return results;
   }
 }
 
